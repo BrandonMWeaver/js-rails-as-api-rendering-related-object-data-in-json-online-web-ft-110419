660cc5b340d8f2be21e3898ba8c9a8d25c6f0506
Brandon Weaver <a7xncob@gmail.com>
2020-02-24 21:51:28 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -6,7 +6,7 @@ class SightingsController < ApplicationController
       render json: sighting, include: {
         bird: { except: [:created_at, :updated_at] },
         location: { except: [:created_at, :updated_at] }
-      }
+      }, except: [:created_at, :updated_at]
     else
       render json: { message: "No sighting found with that id" }
     end
